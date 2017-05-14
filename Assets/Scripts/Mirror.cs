b3824a0f0872440fd23d8822751a022b93792747
AWanderingBear <AWanderingBear@users.noreply.github.com>
2017-05-14 12:33:35 +1200

    Testing Hand Mirroring

---
+++ Assets/Scripts/Mirror.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mirror : MonoBehaviour {
+
+    public bool MirrorToHand = false;
+    public Transform MirrorFromHand;
+    public Transform Player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(MirrorToHand == true)
+        {
+            this.enabled = true; 
+            transform.position = new Vector3(MirrorFromHand.position.x - Player.position.x, transform.position.y, transform.position.z);
+
+        }
+	}
+}
