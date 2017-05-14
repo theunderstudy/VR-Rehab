a288849eba44480e5b27e331f45dd4543cb152c3
Francis <f.joseph.stephens@gmail.com>
2017-05-14 15:50:10 +1200

    UI start

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GameManager : MonoBehaviour {
     public static GameManager instance;
+    public float AugmentMax=5;
+    public float AugmentMin=0;
+    public float AugmentAmount = 2;
+    public Slider AugmentSlider;
+    public Text SliderText;
     // Use this for initialization
     void Awake()
     {
@@ -19,10 +24,26 @@ public class GameManager : MonoBehaviour {
         }
         //set up our singleton instance
         instance = this;
+
+        AugmentSlider.maxValue = AugmentMax;
+        AugmentSlider.minValue = AugmentMin;
+        if (PlayerPrefs.HasKey("AugmentSlider"))
+        {
+            AugmentSlider.value = PlayerPrefs.GetInt("AugmentSlider");
+        }
+        else
+        {
+            AugmentSlider.value = AugmentAmount;
+        }
     }
-	
+	public void Slide()
+    {
+        AugmentAmount = AugmentSlider.value;
+        PlayerPrefs.SetFloat("AugmentSlider",AugmentAmount);
+        SliderText.text = AugmentSlider.value.ToString("F2");
+    }
 	// Update is called once per frame
 	void Update () {
-		
+       
 	}
 }
---
+++ Assets/Scripts/UIScripts/ButtonBase.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class ButtonBase : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField]
+    protected string Function;
+    public Color Selected;
+    private void Start()
+    {
+        if (Function == "")
+        {
+            Function = gameObject.name;
+        }
+    }
+    public virtual void OnPointerDown(PointerEventData eventData)
+    {
+      
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+
+            Invoke(Function ,0);
+           ButtonBase[] buttons = FindObjectsOfType<ButtonBase>();//
+            foreach (ButtonBase item in buttons)
+            {
+                item.GetComponent<Image>().color = Color.white;
+            }
+            GetComponent<Image>().color = Selected;
+        }           
+        else if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            Debug.Log("right click");
+        }
+    }
+    public virtual void OnPointerUp(PointerEventData eventData)
+    {
+
+    }
+    public virtual void OnPointerEnter(PointerEventData eventData)
+    {
+        //tweenUp
+        transform.DOScale(Vector3.one*1.2f, 0.05f);
+    }
+    public virtual void OnPointerExit(PointerEventData eventData)
+    {
+        transform.DOScale(Vector3.one, 0.05f);
+
+        //TweenDown
+    }
+
+    private void Reset()
+    {
+
+    }
+
+    private void MirrorLeftHand()
+    {
+
+    }
+
+    private void MirrorRightHand()
+    {
+
+    }
+
+    private void AugmentLeftHand()
+    {
+
+    }
+
+    private void AugmentRightHand()
+    {
+
+    }
+
+    private void AugmentBothHands()
+    {
+
+    }
+}
